6dd96e3fee9402481c4909430faef6994a25f036
Noureen Khaled <noureenkhaled2004@gmail.com>
2025-08-13 02:53:36 +0300

    Fix import path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import SearchBar from '../components/searchBar';
+import SearchBar from '../components/Sgit add .earchBar';
 import MovieList from '../components/MovieList';
 import logo from '../assets/logo.png';
 import '../styles/Home.css';
@@ -9,7 +9,7 @@ function Home(){
     const [movies,setMovies] = useState([]);
     const fetchMovies = async (query) => {
         const res = await fetch(`https://www.omdbapi.com/?apikey=${import.meta.env.VITE_OMDB_API_KEY}&s=${query}`); {/*fetches the movie from ombd using the query entered*/}
-        const data = await res.json();
+        const data = await res.json(); 
         setMovies(data.Search || [])
     };
      return (
